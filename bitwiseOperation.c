ef023e0ab2da8ced90ebc3206534b316b4da194c
Boris Cavalier <bbca@eurofins.com>
2023-08-03 19:00:18 +0200

    #123 fix cycle_bits didn't try to avoid arrays, just picked up the formula they gave

--- bitwiseOperation.c
+++ bitwiseOperation.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-/* [S2] Bitwise operation: high order bits
+/* [S2] Bitwise operation: high order _bits
  *
  * https://inginious.info.ucl.ac.be/course/LEPL1503/s2_bits_leftmost
  *
- * Write the body of a function get_3_leftmost_bits that returns the 3 high order bits of x.
+ * Write the body of a function get_3_leftmost_bits that returns the 3 high order _bits of x.
  * For instance, if x=0b11011001..., the function should return a uint8_t containing 0b00000110.
  * If x=0b01100001..., the function should return a uint8_t containing 0b00000011.
 */
@@ -14,11 +14,11 @@ uint8_t get_3_leftmost_bits(uint32_t x) {
     return x >> (32 - 3);
 }
 
-/* [S2] Bitwise operation: extract low order bits
+/* [S2] Bitwise operation: extract low order _bits
  *
  * https://inginious.info.ucl.ac.be/course/LEPL1503/s2_bits_rightmost
  *
- * Write the body of a function get_4_rightmost_bits that returns the 4 low order bits of x.
+ * Write the body of a function get_4_rightmost_bits that returns the 4 low order _bits of x.
  * For instance, if x=0b...11011001, the function should return a uint8_t containing 0b00001001.
  *  If x=0b...11011101, your function should return 0b00001101.
  */
@@ -52,27 +52,27 @@ uint64_t set_bit(uint64_t x, int pos, bool value) {
     return (x >> (pos + 1) << 1 | value) << pos | x << (64 - pos) >> (64 - pos);
 }
 
-/* [S2] Bitwise operation: cycling bits
+/* [S2] Bitwise operation: cycling _bits
  *
  * https://inginious.info.ucl.ac.be/course/LEPL1503/s2_bits_spin
  *
- * In this exercise, we will work with operation on bits. When we speak about the position of a bit,
+ * In this exercise, we will work with operation on _bits. When we speak about the position of a bit,
  * index 0 corresponds to lowest order bit, 1 to the second-lowest order bit, ...
  * In C source code, you can write a number in binary (base 2) by prefixing it via 0b., e.g. 0b11010 = 26.
  *
- * This exercise will introduce some non-standard data types which guarantee that the variable has a fixed number of bits.
+ * This exercise will introduce some non-standard data types which guarantee that the variable has a fixed number of _bits.
  * Indeed, on some machines, a int could use 2, 4 or 8 bytes.
- * Hence, if we want to perform bitwise operations, we have to know first on how many bits we are working.
+ * Hence, if we want to perform bitwise operations, we have to know first on how many _bits we are working.
  *
  * For this, C introduces a new class of variable types :
  *
- * int8_t (signed integer of 8 bits)
- * uint8_t (unsigned integer of 8 bits)
- * uint16_t (unsigned integer of 16 bits)
+ * int8_t (signed integer of 8 _bits)
+ * uint8_t (unsigned integer of 8 _bits)
+ * uint16_t (unsigned integer of 16 _bits)
  * You can mix uint or int with bit-lengths 8, 16, 32 and 64). These types are defined in <stdint.h>
  *
  *
- * Write the body of a function cycle_bits, which cycles all bits from n places to the left,
+ * Write the body of a function cycle_bits, which cycles all _bits from n places to the left,
  *    according to the formula x[(i+n)%32] = x[i], where x[i] is the i-iest bit of x.
  * Here is a simple example with bytes.
  * Consider byte                                  0b0110 1011.
@@ -81,32 +81,68 @@ uint64_t set_bit(uint64_t x, int pos, bool value) {
  *                                                  hgfe dcba
  * If n is set to 4, then cycle_bits would return 0b1011 0110.
  *                                                  efgh abcd
- */
+ */ 
+uint8_t _bits = 8;
 uint32_t cycle_bits(uint32_t x, uint8_t n) {
-    //TODO to fix!
-    const uint8_t bits = 8;
-    uint32_t rc = 0b0;
-    for (int i = 0; i < bits; i++) {
-        uint32_t value = (x >> i & 0b1) << (1 - (n + i) % bits);
-        rc |= value;
+    // in INGInious uncomment the line below and sets _bits to 32
+    uint8_t _bits = 32;
+    bool bitArray[_bits];
+    bool cycledBitArray[_bits];
+    uint32_t result = 0;
+    int exp2 = 0;
+    // initialise each item bitArray
+    for (/*already initialised*/; exp2 < _bits; exp2++) {
+        bitArray[exp2] = (x >> exp2 & 0b1);
     }
-    return rc;
+    // sets cycledBitArray
+    for (exp2 = 0; exp2 < _bits; exp2++) {
+        cycledBitArray[(exp2 + n) % _bits] = bitArray[exp2];
+    }
+    // get the result given the cycledBitArray
+    for (exp2 = 0; exp2 < _bits; exp2++) {
+        result += (cycledBitArray[exp2] << exp2);
+    }
+    return result;
+
+    // the previous code doesn't work into INGInious because <stdbool.h> is not included so here's another way to do it
+    // bitArray and cycledBitArray is fixed to 32 because we are with a uint32_t whose size is 32
+/*
+uint8_t bitArray[32]; // bool[32] would be nice but <stdbool.h> is not included
+uint8_t cycledBitArray[32];// bool[32] would be nice but <stdbool.h> is not included
+uint32_t result = 0;
+int exp2 = 0;
+// initialise each item bitArray
+for (; //already initialised
+    exp2 < 32;
+    exp2++) {
+    bitArray[exp2] = (x >> exp2 & 0b1);
+}
+// sets cycledBitArray given the formula that is given in the exercice
+for (exp2 = 0; exp2 < 32; exp2++) {
+    cycledBitArray[(exp2 + n) % 32] = bitArray[exp2];
+}
+// get the result given the cycledBitArray
+for (exp2 = 0; exp2 < 32; exp2++) {
+    result += (cycledBitArray[exp2] << exp2);
+}
+return result;
+*/
 }
 
-/* [S2] Bitwise operation: counting set bits
+/* [S2] Bitwise operation: counting set _bits
  *
  * https://inginious.info.ucl.ac.be/course/LEPL1503/s2_bits_sum
  *
- * In this exercise, we will work with operation on bits.
+ * In this exercise, we will work with operation on _bits.
  * When we speak about the position of a bit, index 0 corresponds to lowest order bit, 1 to the second-lowest order bit, ...
  *
  * In C source code, you can write a number in binary (base 2) by prefixing it via 0b., e.g. 0b11010 = 26.
  *
- * This exercise will introduce some non-standard data types which guarantee that the variable has a fixed number of bits.
+ * This exercise will introduce some non-standard data types which guarantee that the variable has a fixed number of _bits.
  * Indeed, on some machines, a int could use 2, 4 or 8 bytes.
- * Hence, if we want to perform bitwise operations, we have to know first on how many bits we are working.
+ * Hence, if we want to perform bitwise operations, we have to know first on how many _bits we are working.
  *
- * Write the body of a function that counts the number of bits set to 1 in its parameter :
+ * Write the body of a function that counts the number of _bits set to 1 in its parameter :
  *
  * For example, with bytes you should obtain the following results:
  *
@@ -124,13 +160,13 @@ uint8_t nbits(uint32_t n) {
  *
  *  https://inginious.info.ucl.ac.be/course/LEPL1503/s2_bits_strong
  *
- * In this exercise, we will work with operation on bits.
+ * In this exercise, we will work with operation on _bits.
  * When we speak about the position of a bit, index 0 corresponds to lowest order bit, 1 to the second-lowest order bit, ...
  *
  * In C source code, you can write a number in binary (base 2) by prefixing it via 0b., e.g. 0b11010 = 26.
- * This exercise will introduce some non-standard data types which guarantee that the variable has a fixed number of bits.
+ * This exercise will introduce some non-standard data types which guarantee that the variable has a fixed number of _bits.
  * Indeed, on some machines, a int could use 2, 4 or 8 bytes.
- * Hence, if we want to perform bitwise operations, we have to know first on how many bits we are working.
+ * Hence, if we want to perform bitwise operations, we have to know first on how many _bits we are working.
  *
  * Write the body of a function reset_highestorder_bit,
  * which sets to 0 the highest bit set to 1 it founds,
@@ -147,15 +183,15 @@ uint8_t nbits(uint32_t n) {
  *  ********
  */
 uint32_t reset_highestorder_strong_bit(uint32_t x) {
-    uint8_t bits = 32;
-    uint8_t strongestBitPosition = bits - 1;
+    uint8_t _bits = 32;
+    uint8_t strongestBitPosition;
     // loop from left to right until...
-    for (strongestBitPosition = bits - 1; strongestBitPosition > 0; strongestBitPosition--) {
+    for (strongestBitPosition = _bits - 1; strongestBitPosition > 0; strongestBitPosition--) {
         // we break the loop if we find 1
         if ((x >> strongestBitPosition) & 0b1) break;
     }
-    // once we have reached our strongest bit position, we can then shift twice to remove it-keeping the remaining bits.
-    return x << (bits - strongestBitPosition) >> (bits - strongestBitPosition);
+    // once we have reached our strongest bit position, we can then shift twice to remove it-keeping the remaining _bits.
+    return x << (_bits - strongestBitPosition) >> (_bits - strongestBitPosition);
 }
 
 void check_set_bit() {
@@ -220,63 +256,65 @@ void check_cycle_bits() {
     uint32_t x = 0b00000000000000000000000000000000;
     x = 0b01101011;
 
-    printf("cycle_bits( %llu, 1) = %llu\n", x, cycle_bits(x, 1));
-    printf("cycle_bits( %llu, 4) = %llu\n", x, cycle_bits(x, 4));
+    printf("[%dbits] cycle_bits( %llu, 1) = %llu\n", _bits, x, cycle_bits(x, 1));
+    printf("[%dbits] cycle_bits( %llu, 4) = %llu\n", _bits, x, cycle_bits(x, 4));
+
+    _bits = 32;
     x = 0b10000000000000000000000000000000;
-    printf("cycle_bits( %llu, 4) = %llu\n", x, cycle_bits(x, 4));
-    printf("cycle_bits( %llu, 4) = %llu\n", x, cycle_bits(x, 4));
-    printf("cycle_bits( %llu, 0) = %llu\n", x, cycle_bits(x, 0));
-    printf("cycle_bits( %llu, 0) = %llu\n", x, cycle_bits(x, 0));
-    printf("cycle_bits( %llu, 63) = %llu\n", x, cycle_bits(x, 63));
-    printf("cycle_bits( %llu, 63) = %llu\n", x, cycle_bits(x, 63));
-    printf("cycle_bits( %llu, 1) = %llu\n", x, cycle_bits(x, 1));
-    printf("cycle_bits( %llu, 1) = %llu\n", x, cycle_bits(x, 1));
-    printf("cycle_bits( %llu, 62) = %llu\n", x, cycle_bits(x, 62));
-    printf("cycle_bits( %llu, 62) = %llu\n", x, cycle_bits(x, 62));
+    printf("[%dbits] cycle_bits( %llu, 4) = %llu\n", _bits, x, cycle_bits(x, 4));
+    printf("[%dbits] cycle_bits( %llu, 4) = %llu\n", _bits, x, cycle_bits(x, 4));
+    printf("[%dbits] cycle_bits( %llu, 0) = %llu\n", _bits, x, cycle_bits(x, 0));
+    printf("[%dbits] cycle_bits( %llu, 0) = %llu\n", _bits, x, cycle_bits(x, 0));
+    printf("[%dbits] cycle_bits( %llu, 63) = %llu\n", _bits, x, cycle_bits(x, 63));
+    printf("[%dbits] cycle_bits( %llu, 63) = %llu\n", _bits, x, cycle_bits(x, 63));
+    printf("[%dbits] cycle_bits( %llu, 1) = %llu\n", _bits, x, cycle_bits(x, 1));
+    printf("[%dbits] cycle_bits( %llu, 1) = %llu\n", _bits, x, cycle_bits(x, 1));
+    printf("[%dbits] cycle_bits( %llu, 62) = %llu\n", _bits, x, cycle_bits(x, 62));
+    printf("[%dbits] cycle_bits( %llu, 62) = %llu\n", _bits, x, cycle_bits(x, 62));
     x = 0b10000000000000000000000000000000;
-    printf("cycle_bits( %llu, 4) = %llu\n", x, cycle_bits(x, 4));
-    printf("cycle_bits( %llu, 4) = %llu\n", x, cycle_bits(x, 4));
-    printf("cycle_bits( %llu, 0) = %llu\n", x, cycle_bits(x, 0));
-    printf("cycle_bits( %llu, 0) = %llu\n", x, cycle_bits(x, 0));
-    printf("cycle_bits( %llu, 63) = %llu\n", x, cycle_bits(x, 63));
-    printf("cycle_bits( %llu, 63) = %llu\n", x, cycle_bits(x, 63));
-    printf("cycle_bits( %llu, 1) = %llu\n", x, cycle_bits(x, 1));
-    printf("cycle_bits( %llu, 1) = %llu\n", x, cycle_bits(x, 1));
-    printf("cycle_bits( %llu, 62) = %llu\n", x, cycle_bits(x, 62));
-    printf("cycle_bits( %llu, 62) = %llu\n", x, cycle_bits(x, 62));
+    printf("[%dbits] cycle_bits( %llu, 4) = %llu\n", _bits, x, cycle_bits(x, 4));
+    printf("[%dbits] cycle_bits( %llu, 4) = %llu\n", _bits, x, cycle_bits(x, 4));
+    printf("[%dbits] cycle_bits( %llu, 0) = %llu\n", _bits, x, cycle_bits(x, 0));
+    printf("[%dbits] cycle_bits( %llu, 0) = %llu\n", _bits, x, cycle_bits(x, 0));
+    printf("[%dbits] cycle_bits( %llu, 63) = %llu\n", _bits, x, cycle_bits(x, 63));
+    printf("[%dbits] cycle_bits( %llu, 63) = %llu\n", _bits, x, cycle_bits(x, 63));
+    printf("[%dbits] cycle_bits( %llu, 1) = %llu\n", _bits, x, cycle_bits(x, 1));
+    printf("[%dbits] cycle_bits( %llu, 1) = %llu\n", _bits, x, cycle_bits(x, 1));
+    printf("[%dbits] cycle_bits( %llu, 62) = %llu\n", _bits, x, cycle_bits(x, 62));
+    printf("[%dbits] cycle_bits( %llu, 62) = %llu\n", _bits, x, cycle_bits(x, 62));
     x = 0b00000000000000000000000000111111;
-    printf("cycle_bits( %llu, 4) = %llu\n", x, cycle_bits(x, 4));
-    printf("cycle_bits( %llu, 4) = %llu\n", x, cycle_bits(x, 4));
-    printf("cycle_bits( %llu, 0) = %llu\n", x, cycle_bits(x, 0));
-    printf("cycle_bits( %llu, 0) = %llu\n", x, cycle_bits(x, 0));
-    printf("cycle_bits( %llu, 63) = %llu\n", x, cycle_bits(x, 63));
-    printf("cycle_bits( %llu, 63) = %llu\n", x, cycle_bits(x, 63));
+    printf("[%dbits] cycle_bits( %llu, 4) = %llu\n", _bits, x, cycle_bits(x, 4));
+    printf("[%dbits] cycle_bits( %llu, 4) = %llu\n", _bits, x, cycle_bits(x, 4));
+    printf("[%dbits] cycle_bits( %llu, 0) = %llu\n", _bits, x, cycle_bits(x, 0));
+    printf("[%dbits] cycle_bits( %llu, 0) = %llu\n", _bits, x, cycle_bits(x, 0));
+    printf("[%dbits] cycle_bits( %llu, 63) = %llu\n", _bits, x, cycle_bits(x, 63));
+    printf("[%dbits] cycle_bits( %llu, 63) = %llu\n", _bits, x, cycle_bits(x, 63));
     x = 0b00000000000000000000000011111111;
-    printf("cycle_bits( %llu, 0) = %llu\n", x, cycle_bits(x, 0));
-    printf("cycle_bits( %llu, 0) = %llu\n", x, cycle_bits(x, 0));
-    printf("cycle_bits( %llu, 1) = %llu\n", x, cycle_bits(x, 1));
-    printf("cycle_bits( %llu, 1) = %llu\n", x, cycle_bits(x, 1));
-    printf("cycle_bits( %llu, 2) = %llu\n", x, cycle_bits(x, 2));
-    printf("cycle_bits( %llu, 2) = %llu\n", x, cycle_bits(x, 2));
-    printf("cycle_bits( %llu, 3) = %llu\n", x, cycle_bits(x, 3));
-    printf("cycle_bits( %llu, 3) = %llu\n", x, cycle_bits(x, 3));
-    printf("cycle_bits( %llu, 4) = %llu\n", x, cycle_bits(x, 4));
-    printf("cycle_bits( %llu, 4) = %llu\n", x, cycle_bits(x, 4));
-    printf("cycle_bits( %llu, 5) = %llu\n", x, cycle_bits(x, 5));
-    printf("cycle_bits( %llu, 5) = %llu\n", x, cycle_bits(x, 5));
-    printf("cycle_bits( %llu, 6) = %llu\n", x, cycle_bits(x, 6));
-    printf("cycle_bits( %llu, 6) = %llu\n", x, cycle_bits(x, 6));
-    printf("cycle_bits( %llu, 7) = %llu\n", x, cycle_bits(x, 7));
-    printf("cycle_bits( %llu, 7) = %llu\n", x, cycle_bits(x, 7));
-    printf("cycle_bits( %llu, 8) = %llu\n", x, cycle_bits(x, 8));
-    printf("cycle_bits( %llu, 8) = %llu\n", x, cycle_bits(x, 8));
-    printf("cycle_bits( %llu, 9) = %llu\n", x, cycle_bits(x, 9));
-    printf("cycle_bits( %llu, 9) = %llu\n", x, cycle_bits(x, 9));
-    printf("cycle_bits( %llu, 10) = %llu\n", x, cycle_bits(x, 10));
-    printf("cycle_bits( %llu, 10) = %llu\n", x, cycle_bits(x, 10));
+    printf("[%dbits] cycle_bits( %llu, 0) = %llu\n", _bits, x, cycle_bits(x, 0));
+    printf("[%dbits] cycle_bits( %llu, 0) = %llu\n", _bits, x, cycle_bits(x, 0));
+    printf("[%dbits] cycle_bits( %llu, 1) = %llu\n", _bits, x, cycle_bits(x, 1));
+    printf("[%dbits] cycle_bits( %llu, 1) = %llu\n", _bits, x, cycle_bits(x, 1));
+    printf("[%dbits] cycle_bits( %llu, 2) = %llu\n", _bits, x, cycle_bits(x, 2));
+    printf("[%dbits] cycle_bits( %llu, 2) = %llu\n", _bits, x, cycle_bits(x, 2));
+    printf("[%dbits] cycle_bits( %llu, 3) = %llu\n", _bits, x, cycle_bits(x, 3));
+    printf("[%dbits] cycle_bits( %llu, 3) = %llu\n", _bits, x, cycle_bits(x, 3));
+    printf("[%dbits] cycle_bits( %llu, 4) = %llu\n", _bits, x, cycle_bits(x, 4));
+    printf("[%dbits] cycle_bits( %llu, 4) = %llu\n", _bits, x, cycle_bits(x, 4));
+    printf("[%dbits] cycle_bits( %llu, 5) = %llu\n", _bits, x, cycle_bits(x, 5));
+    printf("[%dbits] cycle_bits( %llu, 5) = %llu\n", _bits, x, cycle_bits(x, 5));
+    printf("[%dbits] cycle_bits( %llu, 6) = %llu\n", _bits, x, cycle_bits(x, 6));
+    printf("[%dbits] cycle_bits( %llu, 6) = %llu\n", _bits, x, cycle_bits(x, 6));
+    printf("[%dbits] cycle_bits( %llu, 7) = %llu\n", _bits, x, cycle_bits(x, 7));
+    printf("[%dbits] cycle_bits( %llu, 7) = %llu\n", _bits, x, cycle_bits(x, 7));
+    printf("[%dbits] cycle_bits( %llu, 8) = %llu\n", _bits, x, cycle_bits(x, 8));
+    printf("[%dbits] cycle_bits( %llu, 8) = %llu\n", _bits, x, cycle_bits(x, 8));
+    printf("[%dbits] cycle_bits( %llu, 9) = %llu\n", _bits, x, cycle_bits(x, 9));
+    printf("[%dbits] cycle_bits( %llu, 9) = %llu\n", _bits, x, cycle_bits(x, 9));
+    printf("[%dbits] cycle_bits( %llu, 10) = %llu\n", _bits, x, cycle_bits(x, 10));
+    printf("[%dbits] cycle_bits( %llu, 10) = %llu\n", _bits, x, cycle_bits(x, 10));
 }
 
-void check_reset_highestorder_strong_bit(){
+void check_reset_highestorder_strong_bit() {
     uint32_t x = 0b000111010101010;
     printf("reset_highestorder_strong_bit(%d 0b000111010101010) = %d\n", x, reset_highestorder_strong_bit(x));
     x = 0b0001;
@@ -293,4 +331,5 @@ void check_reset_highestorder_strong_bit(){
 
 int main(int argc, char **argv) {
 // call check_XXX if you want some outputs
+check_cycle_bits();
 }
