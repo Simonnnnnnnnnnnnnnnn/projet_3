06aa74c0d5d7dbf9303ade9c93c159697177fe65
Boris Cavalier <bbca@eurofins.com>
2023-08-03 12:45:04 +0200

    #123 fix bitwiseOperation.set_bit

--- bitwiseOperation.c
+++ bitwiseOperation.c
@@ -1,9 +1,33 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdbool.h>
-#include <inttypes.h>
 
-/*
+/* [S2] Bitwise operation: high order bits
+ *
+ * https://inginious.info.ucl.ac.be/course/LEPL1503/s2_bits_leftmost
+ * Write the body of a function get_3_leftmost_bits that returns the 3 high order bits of x.
+ * For instance, if x=0b11011001..., the function should return a uint8_t containing 0b00000110.
+ * If x=0b01100001..., the function should return a uint8_t containing 0b00000011.
+*/
+uint8_t get_3_leftmost_bits(uint32_t x) {
+    return x >> (32-3);
+}
+
+/* [S2] Bitwise operation: extract low order bits
+ *
+ * https://inginious.info.ucl.ac.be/course/LEPL1503/s2_bits_rightmost
+ * Write the body of a function get_4_rightmost_bits that returns the 4 low order bits of x.
+ * For instance, if x=0b...11011001, the function should return a uint8_t containing 0b00001001.
+ *  If x=0b...11011101, your function should return 0b00001101.
+ */
+uint8_t get_4_rightmost_bits(uint32_t x) {
+    return x & 0b1111;
+}
+
+
+/* [S2] Bitwise operation: setting a bit
+ *
+ * https://inginious.info.ucl.ac.be/course/LEPL1503/s2_bits_set
  * Write the body of a function that returns the value of variable x, but modified with the bit at position pos set to value.
  * Remember that in C (this is defined in stdbool.h), true corresponds to integer 1 while false corresponds to integer 0.
  * Remember than in C, you can create a mask as a binary value and use it with the bitwise and (&) and or (|) operations.
@@ -19,12 +43,10 @@
  *       // (d&c) returns 0b11011001
  *
  * @pre 0<= pos < 64
- *  example:
+ *  example: see main
 */
 uint64_t set_bit(uint64_t x, int pos, bool value) {
-    if (pos == 0) { return x >> 1 << 1 | value; }
-    if (pos == 63) { return (0b1000000000000000000000000000000000000000000000000000000000000000 * value) | x << 1 >> 1 ;}
-    return ((((x >> pos) << 1) | value) << (pos - 1)) | ((x << (64 - pos)) >> (65 - pos));
+    return (x >> (pos+1) << 1 | value) << pos | x << (64-pos) >> (64-pos);
 }
 
 int main(int argc, char **argv) {
@@ -60,7 +82,27 @@ int main(int argc, char **argv) {
     printf("set_bit( %llu, 0, false) = %llu\n", x, set_bit(x, 0, false));
     printf("set_bit( %llu, 63, true) = %llu\n", x, set_bit(x, 63, true));
     printf("set_bit( %llu, 63, false) = %llu\n", x, set_bit(x, 63, false));
-    x=0b11111111;
+    x=0b0000000000000000000000000000000000000000000000000000000011111111;
+    printf("set_bit( %llu, 0, true) = %llu\n", x, set_bit(x, 0, true));
+    printf("set_bit( %llu, 0, false) = %llu\n", x, set_bit(x, 0, false));
+    printf("set_bit( %llu, 1, true) = %llu\n", x, set_bit(x, 1, true));
+    printf("set_bit( %llu, 1, false) = %llu\n", x, set_bit(x, 1, false));
+    printf("set_bit( %llu, 2, true) = %llu\n", x, set_bit(x, 2, true));
+    printf("set_bit( %llu, 2, false) = %llu\n", x, set_bit(x, 2, false));
+    printf("set_bit( %llu, 3, true) = %llu\n", x, set_bit(x, 3, true));
+    printf("set_bit( %llu, 3, false) = %llu\n", x, set_bit(x, 3, false));
+    printf("set_bit( %llu, 4, true) = %llu\n", x, set_bit(x, 4, true));
+    printf("set_bit( %llu, 4, false) = %llu\n", x, set_bit(x, 4, false));
+    printf("set_bit( %llu, 5, true) = %llu\n", x, set_bit(x, 5, true));
+    printf("set_bit( %llu, 5, false) = %llu\n", x, set_bit(x, 5, false));
+    printf("set_bit( %llu, 6, true) = %llu\n", x, set_bit(x, 6, true));
+    printf("set_bit( %llu, 6, false) = %llu\n", x, set_bit(x, 6, false));
+    printf("set_bit( %llu, 7, true) = %llu\n", x, set_bit(x, 7, true));
+    printf("set_bit( %llu, 7, false) = %llu\n", x, set_bit(x, 7, false));
     printf("set_bit( %llu, 8, true) = %llu\n", x, set_bit(x, 8, true));
     printf("set_bit( %llu, 8, false) = %llu\n", x, set_bit(x, 8, false));
+    printf("set_bit( %llu, 9, true) = %llu\n", x, set_bit(x, 9, true));
+    printf("set_bit( %llu, 9, false) = %llu\n", x, set_bit(x, 9, false));
+    printf("set_bit( %llu, 10, true) = %llu\n", x, set_bit(x, 10, true));
+    printf("set_bit( %llu, 10, false) = %llu\n", x, set_bit(x, 10, false));
 }
